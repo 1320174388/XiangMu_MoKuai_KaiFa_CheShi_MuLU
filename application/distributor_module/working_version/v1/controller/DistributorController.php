64bd6cf09db46f4151a133fc4091cb784348a0ba
冯天水 <fengtianshui@dlaotianhuang.com>
2018-08-30 09:49:16 +0800

    2018/8/30 09:08 创建分销模块

--- application/distributor_module/working_version/v1/controller/DistributorController.php
+++ application/distributor_module/working_version/v1/controller/DistributorController.php
@@ -13,5 +13,29 @@ use app\distributor_module\working_version\v1\service\DistributorService;
 
 class DistributorController extends Controller
 {
+    /**
+     * 名  称 : distributionPost()
+     * 功  能 : 创建分销员接口
+     * 变  量 : --------------------------------------
+     * 输  入 : (string) $userToken       => `用户token标识`
+     * 输  入 : (string) $parentToken     => `上级token标识`
+     * 输  入 : (string) $userName        => `用户昵称`
+     * 输  入 : (string) $userImage       => `用户头像图片路径`
+     * 输  出 : {"errNum":0,"retMsg":"提示信息","retData":true}
+     * 创  建 : 2018/08/27 18:06
+     */
+    public function distributionPost(\think\Request $request)
+    {
+        // 实例化Service层逻辑类
+        $distributionService = new DistributorService();
 
+        // 获取传入参数
+        $post = $request->post();
+
+        // 执行Service逻辑
+        $res = $distributionService->distributionAdd($post);
+
+        // 处理函数返回值
+        return \RSD::wxReponse($res,'S','');
+    }
 }
--- application/distributor_module/working_version/v1/service/DistributorService.php
+++ application/distributor_module/working_version/v1/service/DistributorService.php
@@ -13,5 +13,28 @@ use app\distributor_module\working_version\v1\library\DistributorLibrary;
 
 class DistributorService
 {
+    /**
+     * 名  称 : distributionAdd()
+     * 功  能 : 创建分销员接口
+     * 变  量 : --------------------------------------
+     * 输  入 : (string) $userToken       => `用户token标识`
+     * 输  入 : (string) $parentToken     => `上级token标识`
+     * 输  入 : (string) $userName        => `用户昵称`
+     * 输  入 : (string) $userImage       => `用户头像图片路径`
+     * 输  出 : ['msg'=>'success','data'=>'提示信息']
+     * 创  建 : 2018/08/27 18:06
+     */
+    public function distributionAdd($post)
+    {
+        //验证数据
 
+        // 实例化Dao层数据类
+        $distributionDao = new DistributionDao();
+
+        // 执行Dao层逻辑
+        $res = $distributionDao->distributionCreate($post);
+
+        // 处理函数返回值
+        return \RSD::wxReponse($res,'D');
+    }
 }
